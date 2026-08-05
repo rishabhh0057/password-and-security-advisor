3813d64b51fe9617dd6463cfa2fed0f2c54bcf51
rishabhh0057 <rishabhh0057@gmail.com>
2026-08-05 12:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import requests
 import pandas as pd
 import streamlit as st
 import altair as alt
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 
